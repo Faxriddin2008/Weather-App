aca18c3c3c371ab7c5de6156ab153a58fed17c6a
Faxriddin2008 <fabduvasiyev@gmail.com>
2023-09-10 22:21:56 +0500

    feat: add basic code

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { Image } from "react-native";
+import Loader from "./components/loader";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "yellow",
+        }}
+      >
+        <ActivityIndicator size="large" />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/loader.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+// import AnimatedLoader from "react-native-animated-loader";
+import AnimatedLoader from "react-native-animated-loader";
+import { ActivityIndicator, View } from "react-native";
+
+const Loader = () => {
+  //   return (
+  // <AnimatedLoader
+  //   visible={true}
+  //   overlayColor="#FDF6AA"
+  //   source={require("../assets/loader2.json")}
+  //   animationStyle={styles.lottie}
+  //   speed={1}
+  //   loop={true}
+  // ></AnimatedLoader>
+  //   );
+};
+
+const styles = StyleSheet.create({
+  lottie: {
+    width: 200,
+    height: 200,
+  },
+});
+export default Loader;
